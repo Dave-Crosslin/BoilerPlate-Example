8528860ef0c9a8d68b6c59aa6b1a7a11bb4a78f5
Dave-Crosslin <concertguitar327@gmail.com>
2018-08-11 02:21:40 -0400

    adding components

---
+++ eCommerceApp/src/shared/component/page/home.jsx
@@ -0,0 +1,24 @@
+// @flow
+
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import {APP_NAME} from '../../config'
+
+const HomePage = () =>
+  <div>
+    <Helmetform
+      meta={[
+        { name: 'header', content:'Landing Page!' },
+        { content: APP_NAME},
+      ]}
+      />
+      <div className="jumbotron">
+        <div className="container">
+          <h1 className="display-3 mb-5">{APP_NAME}</h1>
+        </div>
+      </div>
+        <p>
+          <button type="button" role="button" className="btn-primary">CLICK ME</button>
+        </p>
+      </div>
